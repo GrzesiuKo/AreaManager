0fa47b6237fc255a14c41e740ff38b40e7ac804c
GrzesiuKo <gak@autograf.pl>
2018-12-28 23:57:24 +0100

    Reorganized class Edge

--- Project/src/Diagram/Edge.java
+++ Project/src/Diagram/Edge.java
@@ -3,9 +3,15 @@ package Diagram;
 import Common.Point;
 
 public class Edge {
-    Point start;
-    Point end;
-    Point direction;
+    private Point start;
+    private Point end;
+    private Edge sister;
 
+    public Edge(Point start) {
+        this.start = start;
+    }
 
+    public void setSister(Edge sister) {
+        this.sister = sister;
+    }
 }
