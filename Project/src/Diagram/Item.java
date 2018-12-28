a5f202da55dbbceabd225aa64bbfaac3ba4acb89
GrzesiuKo <gak@autograf.pl>
2018-12-28 21:53:12 +0100

    Added methods for finding nearest Arcs to the Item

--- Project/src/Diagram/Item.java
+++ Project/src/Diagram/Item.java
@@ -5,6 +5,27 @@ public class Item {
     private Item left;
     private Item right;
 
+    public Item getNearestLeftArc(){
+        Item result = left;
+
+        if (left != null){
+            while(result instanceof Cross){
+                result = result.getRight();
+            }
+        }
+        return result;
+    }
+
+    public Item getNearestRightArc(){
+        Item result = right;
+
+        if (right != null){
+            while(result instanceof Cross){
+                result = result.getLeft();
+            }
+        }
+        return result;
+    }
 
     public Item getParent() {
         return parent;
