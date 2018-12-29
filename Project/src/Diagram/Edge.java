c64e259d85d4bd72a950c4e10952b1e08d4ab232
GrzesiuKo <gak@autograf.pl>
2018-12-29 20:15:05 +0100

    Added finding line formula of the Edge

--- Project/src/Diagram/Edge.java
+++ Project/src/Diagram/Edge.java
@@ -1,17 +1,35 @@
 package Diagram;
 
+import Common.Math;
 import Common.Point;
 
 public class Edge {
     private Point start;
     private Point end;
     private Edge sister;
+    private double slope;
+    private double InterceptY;
 
-    public Edge(Point start) {
+    public Edge(Point start, Point a, Point b) {
         this.start = start;
+        findEdgeFormula(a, b);
     }
 
     public void setSister(Edge sister) {
         this.sister = sister;
+    }
+
+    private void findEdgeFormula(Point a, Point b){
+        Math.TwoResults data = Math.findBisector(a, b);
+        slope = data.getFirst();
+        InterceptY = data.getSecond();
+    }
+
+    public double getSlope() {
+        return slope;
+    }
+
+    public double getInterceptY() {
+        return InterceptY;
     }
 }
