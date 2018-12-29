198725f92e8b8e26cc845c77e7837d9881ecd6d7
Arek <ark.michalak@gmail.com>
2018-12-29 02:29:01 +0100

    Statistic is rising

--- Project/src/Statistics/Statistics.java
+++ Project/src/Statistics/Statistics.java
@@ -1,5 +1,7 @@
 package Statistics;
 
+import Common.Point;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,29 +9,66 @@ public class Statistics {
     private List<Bear> bearList;
     private List<School> schoolList;
     private List<Residential> residentialList;
-    private static Statistics instance = null ;
+    private static Statistics instance = null;
 
-    private Statistics(){
+    private Statistics() {
         bearList = new ArrayList<>();
         schoolList = new ArrayList<>();
         residentialList = new ArrayList<>();
     }
 
-    public static Statistics getInstance(){
-        if(instance == null){
-           instance = new Statistics();
+    public static Statistics getInstance() {
+        if (instance == null) {
+            instance = new Statistics();
         }
         return instance;
     }
 
-    public void addSchool(School school){
+    public void addSchool(School school) {
         schoolList.add(school);
     }
-    public void addBear(Bear bear){
+
+    public void addBear(Bear bear) {
         bearList.add(bear);
     }
-    public void addResidentail(Residential residential){
+
+    public void addResidentail(Residential residential) {
         residentialList.add(residential);
+    }
+
+    public void printAllObjectList(Point fromUser) {
+
+
+    }
+
+    public void printGroupObjectList(Point fromUser) {
+
+
+    }
+
+    public void printResidentsNumber(Point fromUser) {
+
+
+    }
+
+    public void recheckData() {
+        
+
+    }
+
+    private Point findKeyPoint(Point fromUser) {
+        List<Point> tmp = new ArrayList<Point>();
+        tmp.add(new Point(1, 1));
+        tmp.add(new Point(100, 100));
+        double distance = Math.sqrt(Math.pow(fromUser.getX() - tmp.get(0).getX(), 2) + Math.pow(fromUser.getY() - tmp.get(0).getY(), 2));
+        Point nearest = tmp.get(0);
+        for (int i = 0; i < tmp.size(); i++) {
+            if (Math.sqrt(Math.pow(fromUser.getX() - tmp.get(i).getX(), 2) + Math.pow(fromUser.getY() - tmp.get(i).getY(), 2)) < distance) {
+                distance = Math.sqrt(Math.pow(fromUser.getX() - tmp.get(i).getX(), 2) + Math.pow(fromUser.getY() - tmp.get(i).getY(), 2));
+                nearest = tmp.get(i);
+            }
+        }
+        return nearest;
     }
 
     @Override
