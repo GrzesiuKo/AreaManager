96f0fad967369f958040f7760636d5714bc0ca09
GrzesiuKo <gak@autograf.pl>
2018-12-30 15:16:24 +0100

    Moved Arc related methods from Item to Arc

--- Project/src/Diagram/Arc.java
+++ Project/src/Diagram/Arc.java
@@ -22,4 +22,41 @@ public class Arc extends Item {
     public void setEvent(Event event) {
         this.event = event;
     }
+
+    public Cross getNearestLeftCross(){
+        Item parent = this.getParent();
+        Item child = this;
+
+        if (parent == null){
+            return null;
+        }
+        while(parent.getLeft() == child){
+            if (parent.getParent() == null){
+                return null;
+            }else{
+                child = parent;
+                parent = parent.getParent();
+            }
+        }
+        return (Cross)parent;
+    }
+
+    public Cross getNearestRightCross(){
+        Item parent = this.getParent();
+        Item child = this;
+
+        if (parent == null){
+            return null;
+        }
+
+        while(parent.getRight() == child){
+            if (parent.getParent() == null){
+                return null;
+            }else{
+                child = parent;
+                parent = parent.getParent();
+            }
+        }
+        return (Cross)parent;
+    }
 }
