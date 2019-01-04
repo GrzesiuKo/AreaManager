7537d8e1c7cc26d392b4de19f7d98b4960cd1496
GrzesiuKo <gak@autograf.pl>
2019-01-04 16:41:13 +0100

    Initialized FileData package

---
+++ Project/src/FileData/FileReader.java
@@ -0,0 +1,17 @@
+package FileData;
+
+import Common.Point;
+import Statistics.UserObject;
+
+import java.io.File;
+import java.util.List;
+
+public class FileReader {
+    private List<Point> keyPoints;
+    private List<UserObject> objects;
+    private List<Point> contourPoints;
+
+    public void readFile(File file){
+
+    }
+}
