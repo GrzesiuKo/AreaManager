556940907ab7d083d2bdd16f380975a02642dd14
GrzesiuKo <gak@autograf.pl>
2019-01-05 11:02:00 +0100

    Prepared Diagram for new Voronoi idea

--- Project/src/Diagram/Diagram.java
+++ Project/src/Diagram/Diagram.java
@@ -2,10 +2,27 @@ package Diagram;
 
 import Common.Contour;
 import Common.Point;
+import FileData.FileChecker;
+import FileData.FileReader;
 
+import java.io.File;
 import java.util.List;
 
 public class Diagram {
-    List<Point> keyPoints;
-    Contour contour;
+    private List<Point> keyPoints;
+    private Contour contour;
+    private Cell[][] area;
+
+
+    public List<Point> getKeyPoints() {
+        return keyPoints;
+    }
+
+    public Contour getContour() {
+        return contour;
+    }
+
+    public Cell[][] getArea() {
+        return area;
+    }
 }
