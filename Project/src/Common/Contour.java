ade7aabfd43691349b03b9e9f576a802f8c473d1
GrzesiuKo <gak@autograf.pl>
2019-01-05 10:49:41 +0100

    Added gist of Contour

--- Project/src/Common/Contour.java
+++ Project/src/Common/Contour.java
@@ -1,5 +1,28 @@
 package Common;
 
-public class Contour {
+import java.util.LinkedList;
+import java.util.List;
 
+public class Contour {
+    private List<Point> contourPoints;
+    private List<Point> ignoredPoints;
+
+    public Contour(List<Point> points){
+        contourPoints = new LinkedList<>();
+        ignoredPoints = new LinkedList<>();
+
+        makeContour(points);
+    }
+
+    private void makeContour(List<Point> points){
+
+    }
+
+    public List<Point> getContourPoints() {
+        return contourPoints;
+    }
+
+    public List<Point> getIgnoredPoints() {
+        return ignoredPoints;
+    }
 }
