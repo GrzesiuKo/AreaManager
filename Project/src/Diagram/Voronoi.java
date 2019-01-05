af23c89be5f9c9925e4aa787e69d3ca4e7d5026f
Arek <ark.michalak@gmail.com>
2019-01-05 12:35:29 +0100

    Naprawione

--- Project/src/Diagram/Voronoi.java
+++ Project/src/Diagram/Voronoi.java
@@ -1,58 +1,6 @@
 package Diagram;
 
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import Common.Point;
 
 public class Voronoi {
-    private List<Edge> edges;
-    private Item root;
-    private Queue<Event> events;
-    public static double currentYofSweepLine;
-
-
-    public void generateDiagram(List<Point> points) {
-        Queue<Event> events;
-        Event currentEvent;
-
-        events = getEvents(points);
-
-        while (!events.isEmpty()){
-            currentEvent = events.poll();
-            currentYofSweepLine = currentEvent.getPoint().getY();
-            if (currentEvent.isSiteEvent()){
-               // handleSiteEvent();
-            }else{
-               // handleCircleEvent();
-            }
-        }
-
-
-    }
-
-    private Queue<Event> getEvents(List<Point> points){
-        List<Point> keyPoints;
-        Queue<Event> events = new PriorityQueue<Event>();
-
-        keyPoints = points;
-
-        keyPoints = sortPoints(keyPoints);
-
-        for (Point p: keyPoints) {
-            events.add(new Event(p, Event.SITE_EVENT));
-        }
-
-        return events;
-    }
-
-    private List<Point> sortPoints(List<Point> points) {
-        List<Point> resultPoints = points;
-
-        resultPoints.sort((one, two) -> (int) (two.getY() - one.getY()));
-
-        return resultPoints;
-    }
-
 
 }
---
+++ Project/src/FileData/FileChecker.java
@@ -0,0 +1,15 @@
+package FileData;
+
+import java.io.File;
+import java.util.List;
+
+public class FileChecker {
+    private List<Integer> errorLines;
+
+
+    public boolean checkFile(File file){
+        boolean result = true;
+
+        return result;
+    }
+}
