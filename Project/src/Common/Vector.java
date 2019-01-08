3faff08140544f4e5561f0ddf6a02fc584e50dfb
GrzesiuKo <gak@autograf.pl>
2019-01-08 18:23:45 +0100

    Added different constructor to Vector
    It allows to make vector out of double numbers

--- Project/src/Common/Vector.java
+++ Project/src/Common/Vector.java
@@ -1,8 +1,12 @@
 package Common;
 
 public class Vector extends Point {
-    public Vector(Point a, Point b) {
-        super(b.getX() - a.getX(), b.getY() - a.getY());
+    public Vector(Point start, Point end) {
+        super(end.getX() - start.getX(), end.getY() - start.getY());
+    }
+
+    public Vector(double start, double end){
+        super(start,end);
     }
 
 
