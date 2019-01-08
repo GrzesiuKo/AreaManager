f9c288413dac8818dcc061bd1621347929311f94
GrzesiuKo <gak@autograf.pl>
2019-01-08 13:29:17 +0100

    Added gist of Vector class

---
+++ Project/src/Common/Vector.java
@@ -0,0 +1,20 @@
+package Common;
+
+public class Vector extends Point {
+    public Vector(Point a, Point b) {
+        super(b.getX()-a.getX(),b.getY()-a.getY());
+    }
+
+
+    private double findCosinusBetween(Vector a, Vector b){
+        //TODO
+        return 0;
+    }
+
+
+    private double scalarProduct(Point a, Point b){
+        //TODO
+        return 0;
+    }
+
+}
