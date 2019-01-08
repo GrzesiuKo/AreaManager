c3d81dbef9f51be8eb693003fa87f70eea64672f
GrzesiuKo <gak@autograf.pl>
2019-01-08 13:33:34 +0100

    Added gist of making Contour

--- Project/src/Common/Contour.java
+++ Project/src/Common/Contour.java
@@ -15,8 +15,64 @@ public class Contour {
     }
 
     private void makeContour(List<Point> points) {
-        contourPoints = points;
+        Point lowest;
+        Point highest;
+
+        lowest = getLowestPoint(points);
+        highest = getHighestPoint(points);
+
+        points = makeRightSide(points, lowest, highest);
+        points = makeLeftSide(points, lowest, highest);
+
+        ignoredPoints = points;
     }
+
+    private Point getLowestPoint(List<Point> points){
+        Point lowest = null;
+
+        if (points==null){
+            return null;
+        }
+        for (Point p: points){
+            lowest = p.getSmaller(lowest);
+        }
+
+        return lowest;
+    }
+
+    private Point getHighestPoint(List<Point> points){
+        Point lowest = null;
+
+        if (points==null){
+            return null;
+        }
+        for (Point p: points){
+            lowest = p.getBigger(lowest);
+        }
+
+        return lowest;
+    }
+
+    private List<Point> makeRightSide(List<Point> points, Point lowest, Point highest){
+        //TODO
+        return null;
+    }
+
+    private List<Point> makeLeftSide(List<Point> points, Point lowest, Point highest){
+        //TODO
+        return null;
+    }
+
+    private Point nextRightPoint(List<Point> points, Point current){
+        //TODO
+        return null;
+    }
+
+    private Point nextLeftPoint(List<Point> points, Point current){
+        //TODO
+        return null;
+    }
+
 
     public List<Point> getContourPoints() {
         return contourPoints;
