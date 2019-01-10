d2f1a3c92d9dc084074bee794af2ca53593ea846
GrzesiuKo <gak@autograf.pl>
2019-01-10 15:51:11 +0100

    Reorganized readObjectDefinitionLine, made a gist

--- Project/src/FileData/FileChecker.java
+++ Project/src/FileData/FileChecker.java
@@ -2,10 +2,7 @@ package FileData;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class FileChecker {
     public final static int UNKNOWN = -100;
@@ -13,9 +10,14 @@ public class FileChecker {
     public final static int INT = 1;
     public final static int DOUBLE = 2;
     public final static int NOT_GIVEN = 3;
+    public final static int X = 1;
+    public final static int Y = 2;
+    public final static int CHOICE = 3;
+    public final static int MAX_NUMBER_OF_ARGUMENTS = 3;
     private static int currentFilePart;
     private int errorLine;
-    private Map<String, Integer> definitions;
+    private static Map<String, Integer> definitions;
+    private static Map<String, LinkedList<Integer>> argumentsOrders;
 
     public FileChecker() {
         definitions = new HashMap<>();
@@ -112,8 +114,8 @@ public class FileChecker {
     public static void readObjectDefinitionLine(String line, Map<String, Integer> definitions) {
         Scanner scanner;
         String name;
-        String typeName;
-        int typeId;
+        String text;
+        int order;
 
         try {
             scanner = new Scanner(line);
@@ -123,13 +125,46 @@ public class FileChecker {
         scanner.next();
         name = scanner.next();
 
-        if (scanner.hasNext()) {
-            typeName = scanner.next();
-            typeId = recognizeTypeByVariableName(typeName);
-        } else {
-            typeId = NOT_GIVEN;
+        addNameToDictionary(name);
+
+        text = scanner.next();
+
+        for (int i = 0; i< MAX_NUMBER_OF_ARGUMENTS; i++) {
+            if (isCoordinateDefinition(text)) {
+                order = recognizeCoordinate(text);
+                argumentsOrders.get(name).add(order);
+                scanner.next();
+            }else{
+                handleUserVariable(name, text, scanner.next());
+            }
+            text = scanner.next();
         }
-        definitions.put(name, typeId);
+
+
+
+    }
+
+    private static void addNameToDictionary(String name){
+        argumentsOrders.put(name, new LinkedList<>());
+        definitions.put(name, UNKNOWN);
+    }
+
+    private static boolean isCoordinateDefinition(String text){
+        return text.matches("(?i)x(?-i)") || text.matches("(?i)y(?-i)");
+    }
+
+    private static int recognizeCoordinate(String name){
+        if (name.matches("(?i)x(?-i)")){
+            return X;
+        }else if (name.matches("(?i)y(?-i)")){
+            return Y;
+        }else{
+            return UNKNOWN;
+        }
+    }
+
+    private static void handleUserVariable(String objectName, String name, String type){
+
     }
 
     private static int recognizeTypeByVariableName(String name) {
