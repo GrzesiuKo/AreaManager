00864a172a1b45044c199381ad93b59ee57fdb2b
GrzesiuKo <gak@autograf.pl>
2019-01-11 15:44:34 +0100

    Made new version of reading object lines

--- Project/src/FileData/FileReader.java
+++ Project/src/FileData/FileReader.java
@@ -13,14 +13,15 @@ public class FileReader {
     private static int currentFilePart;
     private List<KeyPoint> keyPoints;
     private List<Point> contourPoints;
-    private Map<String, Integer> definitions;
+    private Map<String, LinkedList<Integer>> definitions;
     private List<Point> objectPoints;
 
-    public void readFile(File file) {
+    public void readFile(File file, Map<String, LinkedList<Integer>> definitions) {
         Scanner scanner;
         String currentLine;
 
         currentFilePart = 0;
+        this.definitions = definitions;
         initializeFileReader();
 
         try {
@@ -50,7 +51,6 @@ public class FileReader {
             readKeyPointLine(line, keyPoints);
 
         } else if (FileNavigation.isObjectsDefinitionSection(currentFilePart)) {
-            FileChecker.readObjectDefinitionLine(line, definitions);
 
         } else if (FileNavigation.isObjectsSection(currentFilePart)) {
             readObjectLine(line);
@@ -87,43 +87,84 @@ public class FileReader {
 
     private void readObjectLine(String line) {
         Scanner scanner;
-        String name;
-        double x;
-        double y;
-        Point point;
-        Integer type;
+        String value, objectName;
+        double x, y;
+        UserArgument userArgument;
+        LinkedList<Integer> order;
+        int argumentId;
 
-        try {
-            scanner = new Scanner(line);
-        } catch (NullPointerException e) {
-            return;
-        }
+        scanner = new Scanner(line);
+
         scanner.next();
-        name = scanner.next();
-        x = getDoubleFromString(scanner.next());
-        y = getDoubleFromString(scanner.next());
+        objectName = scanner.next();
+        order = new LinkedList<>(definitions.get(objectName));
+        x=-1;
+        y=-1;
+        userArgument = null;
 
-        type = definitions.get(name);
+        while(!order.isEmpty()){
 
-        point = new Point(x, y);
+            argumentId = order.removeFirst();
+            scanner.next();
+            value = scanner.next();
 
-        addObject(point, name, type, scanner);
+            if (argumentId == FileChecker.X){
+                x = handleCoordinate(value);
+            }else if (argumentId == FileChecker.Y){
+                y = handleCoordinate(value);
+            }else if (argumentId == FileChecker.STRING) {
+                userArgument = new UserArgument(argumentId, value);
+            }else{
+                userArgument = handleUserArgumentNoString(argumentId, value);
+            }
+        }
+        addObject(objectName, new Point(x, y), userArgument);
     }
 
-    private void addObject(Point point, String name, int type, Scanner scanner) {
-        if (type == FileChecker.STRING) {
-            objectPoints.add(point);
-            scanner.useDelimiter("$");
-            UserObject.addObject(point, name, scanner.next());
-        } else if (type == FileChecker.DOUBLE) {
-            objectPoints.add(point);
-            //UserObject.addObject(point, name, getDoubleFromString(scanner.next())); //czeka na funkcje od Arkadiusza
-        } else if (type == FileChecker.INT) {
-            objectPoints.add(point);
-            UserObject.addObject(point, name, scanner.nextInt());
-        } else if (type == FileChecker.NOT_GIVEN) {
+    private double handleCoordinate(String value){
+        return getDoubleFromString(value);
+    }
+
+    private UserArgument handleUserArgumentNoString(int id, String value){
+        UserArgument userArgument;
+        double valueDouble;
+        int valueInt;
+
+        switch (id){
+            case FileChecker.DOUBLE:
+                valueDouble = getDoubleFromString(value);
+                userArgument = new UserArgument(id,valueDouble);
+                break;
+            case FileChecker.INT:
+                valueInt = Integer.parseInt(value);
+                userArgument = new UserArgument(id, valueInt);
+                break;
+            default:
+                return null;
+        }
+        return userArgument;
+    }
+
+    private void addObject(String name, Point point, UserArgument userArgument) {
+        int type;
+
+        if (userArgument == null){
             objectPoints.add(point);
             UserObject.addObject(point, name);
+            return;
+        }
+
+        type = userArgument.getId();
+
+        if (type == FileChecker.STRING) {
+            objectPoints.add(point);
+            UserObject.addObject(point, name, userArgument.getString());
+        } else if (type == FileChecker.DOUBLE) {
+            objectPoints.add(point);
+            //UserObject.addObject(point, name, userArgument.getDouble()); //czeka na funkcje od Arkadiusza
+        } else if (type == FileChecker.INT) {
+            objectPoints.add(point);
+            UserObject.addObject(point, name, userArgument.getInt());
         }
     }
 
