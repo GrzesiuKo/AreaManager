7df743b91d4c5e6c8f682cf68ed5fb6db7a83382
GrzesiuKo <gak@autograf.pl>
2019-01-12 18:04:36 +0100

    Added Point unit tests

---
+++ Project/Tests/Common/PointTest.java
@@ -0,0 +1,46 @@
+package Common;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    void scaleCoordinateToInt() {
+        //Given
+        double a = 2.4;
+        int precision = 10;
+        int expected = 24;
+        int result;
+        //When
+        result = Point.scaleCoordinateToInt(a, precision);
+        //Then
+        assertEquals(expected, result);
+
+    }
+
+    @Test
+    void getBigger() {
+        //Given
+        Point a = new Point(20, 30);
+        Point b = new Point(30, 20);
+        Point result;
+        //When
+        result = a.getBigger(b);
+        //Then
+        assertEquals(a, result);
+    }
+
+    @Test
+    void getSmaller() {
+        //Given
+        Point a = new Point(20, 30);
+        Point b = new Point(30, 20);
+        Point result;
+        //When
+        result = a.getSmaller(b);
+        //Then
+        assertEquals(b, result);
+    }
+}
