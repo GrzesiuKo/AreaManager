11b11388c3fa242c87da08108e52888c80460cc1
GrzesiuKo <gak@autograf.pl>
2019-01-12 18:49:05 +0100

    Corrected and cleaned makeArea test

--- Project/Tests/Diagram/VoronoiTest.java
+++ Project/Tests/Diagram/VoronoiTest.java
@@ -25,40 +25,20 @@ class VoronoiTest {
     @Test
     void makeAreas() {
         //Given
-        size = 3;
+        size = 1000;
         KeyPoint a = new KeyPoint(0.4, 0.0);
         KeyPoint b = new KeyPoint(0.0, 0.4);
         keyPoints.add(a);
         keyPoints.add(b);
-        LinkedList<Point> aPoints = new LinkedList<>();
-        aPoints.add(new Point(0.0, 0.0));
-        aPoints.add(new Point(0.0, 0.1));
-        aPoints.add(new Point(0.0, 0.2));
-        aPoints.add(new Point(0.1, 0.0));
-        aPoints.add(new Point(0.2, 0.0));
-        aPoints.add(new Point(0.1, 0.1));
-        LinkedList<Point> bPoints = new LinkedList<>();
-        bPoints.add(new Point(0.3, 0.3));
-        bPoints.add(new Point(0.2, 0.3));
-        bPoints.add(new Point(0.1, 0.3));
-        bPoints.add(new Point(0.3, 0.2));
-        bPoints.add(new Point(0.3, 0.1));
-        bPoints.add(new Point(0.2, 0.2));
-        LinkedList<Point> rest = new LinkedList<>();
-        rest.add(new Point(0.3, 0.0));
-        rest.add(new Point(0.2, 0.1));
-        rest.add(new Point(0.1, 0.2));
-        rest.add(new Point(0.0, 0.3));
-
         //When
         voronoi = new Voronoi(size, keyPoints, null);
         voronoi.makeAreas(size, keyPoints);
         //Then
         for (Point p : a.getAreaPoints()) {
-            assertTrue(findLengthOfSegment(p, a) < findLengthOfSegment(p, b));
+            assertTrue(findLengthOfSegment(p, a) <= findLengthOfSegment(p, b));
         }
         for (Point p : b.getAreaPoints()) {
-            assertTrue(findLengthOfSegment(p, a) > findLengthOfSegment(p, b));
+            assertTrue(findLengthOfSegment(p, a) >= findLengthOfSegment(p, b));
         }
     }
 
