84d2985266fee95aa2957f77a61850ffd03d6c26
GrzesiuKo <gak@autograf.pl>
2019-01-12 21:11:24 +0100

    Improved FileReader readFile test
    FileReader reads only "good files",
    files approved by FileChecker.

--- Project/Tests/FileData/FileReaderTest.java
+++ Project/Tests/FileData/FileReaderTest.java
@@ -1,18 +1,11 @@
 package FileData;
 
-import Common.Point;
+import Exceptions.IncorrectDefinitionUnknownTypeException;
+import Exceptions.IncorrectLineException;
+import Exceptions.IncorrectObjectLineException;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.AnnotatedType;
-import java.lang.reflect.Executable;
-import java.lang.reflect.TypeVariable;
-import java.util.Map;
-import java.util.Scanner;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class FileReaderTest {
 
@@ -22,33 +15,17 @@ class FileReaderTest {
         FileReader fileReader = new FileReader();
         File file = null;
         //Then
-        assertThrows(NullPointerException.class, ()-> fileReader.readFile(file, null));
+        fileReader.readFile(file, null);
     }
 
     @Test
-    void readFileGoodFile() {
+    void readFileGoodFile() throws IncorrectObjectLineException, IncorrectDefinitionUnknownTypeException, IncorrectLineException {
         //Given
         FileReader fileReader = new FileReader();
-        File file = new File("Tests/TestFiles/dobryPlik.txt");
+        FileChecker fileChecker = new FileChecker();
+        File file = new File("Tests" + File.separator + "TestFiles" + File.separator + "GoodFile.txt");
         //When
-        fileReader.readFile(file, null);
-
-        //Then
-//        System.out.println("Contour points:");
-//        for(Point point : fileReader.getContourPoints()) {
-//            System.out.println(point.getX()+" "+point.getY());
-//        }
-//        System.out.println("Key points:");
-//        for(Point point : fileReader.getKeyPoints()) {
-//            System.out.println(point.getX()+" "+point.getY());
-//        }
-//        Map<String, Integer> mapa = fileReader.getDefinitions();
-//        System.out.println("Szkoła: "+mapa.get("SZKOŁA"));
-//        System.out.println("DOM: "+mapa.get("DOM"));
-//        System.out.println("NIEDŹWIEDŹ: "+mapa.get("NIEDŹWIEDŹ"));
-//
-//        System.out.println("ŁOŚ: "+mapa.get("ŁOŚ"));
-
+        fileChecker.checkFile(file);
+        fileReader.readFile(file, fileChecker.getDefinitions());
     }
-
 }
